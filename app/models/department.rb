bbbdfb5442c87bce90876f64be53189d958c2494
Abhishek Singh <abhishekpratap.singh@newgenapps.com>
2014-01-03 10:52:59 +0530

    new branch work added callback

--- app/models/department.rb
+++ app/models/department.rb
@@ -1,4 +1,21 @@
 class Department < ActiveRecord::Base
   has_many :employees, dependent: :destroy
   validates :name, length: {minimum: 5}
+
+  before_validation :normalize_name, on: :create
+  before_create :capitalize_task
+  # after_validation :set_location, on: [ :create, :update ]
+
+  protected
+  def normalize_name
+    self.name = self.name.downcase.titleize
+  end
+
+  def capitalize_task
+    self.task = self.task.split.map(&:capitalize).join(' ')
+  end
+
+  # def set_location
+    # self.location = LocationService.query(self)
+  # end
 end
